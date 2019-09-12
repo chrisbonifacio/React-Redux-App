7c47d2909d1c44f188d2212a75136787dcb9a281
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-12 15:40:05 -0700

    export topAnime actions + ACs
    Co-authored-by: Chris Bonifacio <christopher.bonifacio@gmail.com>

---
+++ anime-redux/src/store/actions/index.js
@@ -0,0 +1,13 @@
+import { 
+    GET_TOP_ANIME_START,
+    GET_TOP_ANIME_SUCCESS,
+    GET_TOP_ANIME_FAILURE,
+    getTopAnime
+ } from './HomePage'
+
+export {
+    GET_TOP_ANIME_START,
+    GET_TOP_ANIME_SUCCESS,
+    GET_TOP_ANIME_FAILURE,
+    getTopAnime
+}
