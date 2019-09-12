dbcdf9abb76953142c54174c8a84c46e88aa8099
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-12 15:40:22 -0700

    add an AnimeCard component
    Co-authored-by: Chris Bonifacio <christopher.bonifacio@gmail.com>

---
+++ anime-redux/src/components/AnimeCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AnimeCard() {
+    return (
+        <div className="Anime-Card">
+            <img src=""/>
+            <h2>Anime Name</h2>
+            <p>Genre</p>
+        </div>
+    )
+}
