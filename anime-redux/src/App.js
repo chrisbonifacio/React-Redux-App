7f2289511ac24931666a5012045389c04521d41a
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-12 15:51:32 -0700

    render top anime
    Co-authored-by: Chris Bonifacio <christopher.bonifacio@gmail.com>

--- anime-redux/src/App.js
+++ anime-redux/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import TopAnime from "./components/HomePage/TopAnime";
 
 export default function App() {
   return (
     <div className="App">
       Epic Anime List
+      <TopAnime />
     </div>
   )
 }
