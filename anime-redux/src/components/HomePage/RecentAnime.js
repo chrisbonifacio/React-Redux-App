ebf6fb4d693775bd6a42eda9c9a9375c33ccd83a
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-12 14:27:25 -0700

    setup folder structure
    Co-authored-by: Chris Bonifacio <christopher.bonifacio@gmail.com>

---
+++ anime-redux/src/components/HomePage/RecentAnime.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RecentAnime() {
+    return <div className="RecentAnime"></div>
+}
---
+++ anime-redux/src/components/HomePage/UpcomingAnime.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function UpcomingAnime() {
+    return <div className="UpcomingAnime"></div>
+}
