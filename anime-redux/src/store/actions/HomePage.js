321ad021ab9389a9ddd6701f5710761f63302e6f
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-12 15:39:33 -0700

    add getTopAnime actions + ACs
    Co-authored-by: Chris Bonifacio <christopher.bonifacio@gmail.com>

---
+++ anime-redux/src/store/actions/HomePage.js
@@ -0,0 +1,18 @@
+export const GET_TOP_ANIME_START = "GET_TOP_ANIME_START"
+export const GET_TOP_ANIME_SUCCESS = "GET_TOP_ANIME_SUCCESS"
+export const GET_TOP_ANIME_FAILURE = "GET_TOP_ANIME_FAILURE"
+
+export const getTopAnime = () => dispatch => {
+    dispatch({ type: GET_TOP_ANIME_START })
+    fetch("https://api.jikan.moe/v3/top/anime")
+        .then(res => {
+            dispatch({ type: GET_TOP_ANIME_SUCCESS })
+            return res.json()
+        })
+        .then(parsedRes => {
+            console.log(parsedRes.top)
+        })
+        .catch(err => {
+            dispatch({ type: GET_TOP_ANIME_FAILURE })
+            console.log(err);
+        })}
