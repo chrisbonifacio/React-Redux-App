7057048416a24d371ee71535cccbac9857215a91
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-13 01:40:53 -0400

    no change

--- anime-redux/src/components/HomePage/TopAnime.js
+++ anime-redux/src/components/HomePage/TopAnime.js
@@ -1,38 +1,42 @@
 import React, { useEffect } from "react"
-import { connect } from 'react-redux'
-import { getTopAnime } from '../../store/actions'
+import { connect } from "react-redux"
+import { getTopAnime } from "../../store/actions"
 
 import AnimeCard from "../AnimeCard"
 
 function TopAnime(props) {
-    
-    useEffect(() => {
-        props.getTopAnime()
-    }, [])
-    
-    return ( 
-        <div className="Recent-Anime">
-            <div className="Grid">
-                {/* map through list of most recent anime */}
-                {/* Props: image_url, title, rank */}
-                {/* Search for recent anime in year 2019 */}
-                {/* Sort by rank */}
-                {
-                    props.isFetching 
-                    ? 'Loading...'
-                    : props.topAnime.slice(0,10).map((anime, index) => 
-                        <AnimeCard key={anime.mal_id} anime={anime}/>
-                    )}                
-            </div>
-        </div>
-    )
+  useEffect(() => {
+    props.getTopAnime()
+  }, [])
+
+  return (
+    <div className="Top-Anime">
+      <h1>Top Anime</h1>
+      <div className="Grid">
+        {/* map through list of most recent anime */}
+        {/* Props: image_url, title, rank */}
+        {/* Search for recent anime in year 2019 */}
+        {/* Sort by rank */}
+        {props.isFetching
+          ? "Loading..."
+          : props.topAnime
+              .slice(0, 10)
+              .map((anime, index) => (
+                <AnimeCard key={anime.mal_id} anime={anime} />
+              ))}
+      </div>
+    </div>
+  )
 }
 
 function mapStateToProps(state) {
-    return {
-        isFetching: state.homePage.isFetching,
-        topAnime: state.homePage.topAnime
-    }
+  return {
+    isFetching: state.homePage.isFetching,
+    topAnime: state.homePage.topAnime
+  }
 }
 
-export default connect(mapStateToProps, {getTopAnime})(TopAnime);
+export default connect(
+  mapStateToProps,
+  { getTopAnime }
+)(TopAnime)
