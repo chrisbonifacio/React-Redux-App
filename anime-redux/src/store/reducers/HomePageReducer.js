6a442a0f94c4d952f27774808039d7cf95b691af
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-13 01:50:21 -0400

    create cases for upcoming anime

--- anime-redux/src/store/reducers/HomePageReducer.js
+++ anime-redux/src/store/reducers/HomePageReducer.js
@@ -1,38 +1,50 @@
-import { 
-    GET_TOP_ANIME_START,
-    GET_TOP_ANIME_SUCCESS,
-    GET_TOP_ANIME_FAILURE
-} from '../actions'
+import {
+  GET_TOP_ANIME_START,
+  GET_TOP_ANIME_SUCCESS,
+  GET_TOP_ANIME_FAILURE,
+  GET_UPCOMING_ANIME_START,
+  GET_UPCOMING_ANIME_SUCCESS,
+  GET_UPCOMING_ANIME_FAILURE
+} from "../actions"
 
 export const initialStateForHomePage = {
-    fetching: false,
-    topAnime: [],
-    upcomingAnime: [],
-    animeByGenre: []
+  fetching: false,
+  topAnime: [],
+  upcomingAnime: [],
+  animeByGenre: []
 }
 
-
 export function homePageReducer(state = initialStateForHomePage, action) {
-    switch(action.type) {
-        case GET_TOP_ANIME_START:
-            return {
-                ...state, 
-                fetching: true
-            }
-            
-        case GET_TOP_ANIME_SUCCESS: 
-            return {
-                ...state,
-                fetching: false,
-                topAnime: state.topAnime.concat(action.payload)
-            }
-            
-        case GET_TOP_ANIME_FAILURE:
-            return {
-                ...state,
-                fetching: false
-            }
-        default: 
-            return state
-    }
+  switch (action.type) {
+    case GET_TOP_ANIME_START:
+      return {
+        ...state,
+        fetching: true
+      }
+
+    case GET_TOP_ANIME_SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        topAnime: state.topAnime.concat(action.payload)
+      }
+
+    case GET_TOP_ANIME_FAILURE:
+      return {
+        ...state,
+        fetching: false
+      }
+    case GET_UPCOMING_ANIME_START:
+      return { ...state, fetching: true }
+    case GET_UPCOMING_ANIME_SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        upcomingAnime: [...state, action.payload]
+      }
+    case GET_UPCOMING_ANIME_FAILURE:
+      return { ...state, fetching: false }
+    default:
+      return state
+  }
 }
