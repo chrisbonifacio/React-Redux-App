06ae4dec347b5c88c4e84b14f553b5af733f7fcc
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-13 01:38:45 -0400

    wrap App in Router

--- anime-redux/src/index.js
+++ anime-redux/src/index.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import { composeWithDevTools } from 'redux-devtools-extension'
-import reduxThunk from 'redux-thunk'
+import React from "react"
+import ReactDOM from "react-dom"
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from "react-redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import reduxThunk from "redux-thunk"
+import { BrowserRouter as Router } from "react-router-dom"
 
-import App from './App'
-import './index.css'
+import App from "./App"
+import "./index.css"
 
-import rootReducer from './store/reducers'
+import rootReducer from "./store/reducers"
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(reduxThunk)));
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById('root')
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(reduxThunk))
 )
 
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+)
