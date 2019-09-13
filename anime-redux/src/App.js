175a5da174027d1ab1f2224e2d07c2ade00169d0
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-13 01:38:18 -0400

    add routes + render NavBar and UpcomingAnime

--- anime-redux/src/App.js
+++ anime-redux/src/App.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import './App.css'
-import TopAnime from "./components/HomePage/TopAnime";
+import React from "react"
+import { Route } from "react-router-dom"
+import "./App.scss"
+import TopAnime from "./components/HomePage/TopAnime"
+import UpcomingAnime from "./components/HomePage/UpcomingAnime"
+import NavBar from "./components/NavBar"
+import Search from "./components/SearchPage/Search"
 
 export default function App() {
   return (
     <div className="App">
-      Epic Anime List
-      <TopAnime />
+      <NavBar />
+      <div className="container">
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return (
+              <>
+                <TopAnime />
+                <UpcomingAnime />
+              </>
+            )
+          }}
+        />
+        <Route
+          path="/search"
+          render={() => {
+            return (
+              <>
+                <Search />
+              </>
+            )
+          }}
+        />
+      </div>
     </div>
   )
 }
