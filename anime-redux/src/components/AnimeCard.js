5972bb4e648ee5001eb77de452ba12f9ff61988d
Chris Bonifacio <christopher.bonifacio@gmail.com>
2019-09-13 01:40:14 -0400

    move rank to top of AnimeCard

--- anime-redux/src/components/AnimeCard.js
+++ anime-redux/src/components/AnimeCard.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react"
 
-export default function AnimeCard({anime}) {
-    return (
-        <div className="Anime-Card">
-            <img src={anime.image_url}/>
-            <h2>{anime.title}</h2>
-            {anime.rank && <p>Rank: {anime.rank}</p>}
-        </div>
-    )
+export default function AnimeCard({ anime }) {
+  return (
+    <div className="Anime-Card">
+      {anime.rank && <p>{anime.rank}</p>}
+      <img src={anime.image_url} />
+      <h2>{anime.title}</h2>
+    </div>
+  )
 }
